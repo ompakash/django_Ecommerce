0ed01ec8134ca66c74fa5f263906b8b711c88e15
omprakash <opcoc3@gmail.com>
2021-07-28 22:00:38 +0530

    input added in html

--- ecommerce/store/urls.py
+++ ecommerce/store/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from store.views import home, login, signup
 
 urlpatterns = [
-    path('', home.index, name='homepage'),
+    path('', home.Index.as_view(), name='homepage'),
     path('signup', signup.Signup.as_view(), name='signup'),
     path('login', login.Login.as_view(), name='login'),
 ]
--- ecommerce/store/views/home.py
+++ ecommerce/store/views/home.py
@@ -1,23 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from store.models.product import Product
 from store.models.category import Category
+from django.views import View
 
 
 # Create your views here.
-def index(request):
-    products = None
-    categories = Category.get_all_categories()
 
-    categoryID = request.GET.get('category')
-    if categoryID:
-        products = Product.get_all_products_by_categoryid(categoryID)
-    else:
-        products = Product.get_all_products()
+class Index(View):
 
-    data = {}
-    data['products'] = products
-    data['categories'] = categories
-    print(request.session.get('email'))
+    def post(self,request):
+        product = request.POST.get('product')
+        print(product)
+        return redirect('homepage')
 
-    return render(request, 'index.html', data)
+    def get(self,request):
+        products = None
+        categories = Category.get_all_categories()
+
+        categoryID = request.GET.get('category')
+        if categoryID:
+            products = Product.get_all_products_by_categoryid(categoryID)
+        else:
+            products = Product.get_all_products()
+
+        data = {}
+        data['products'] = products
+        data['categories'] = categories
+        print(request.session.get('email'))
+
+        return render(request, 'index.html', data)
+
 
